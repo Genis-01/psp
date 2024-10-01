b6a329d9c02037f0abff53a383321482770f303b
Genis <114994377+Genis-01@users.noreply.github.com>
2024-10-01 17:15:32 +0200

    class

--- src/actividades/unoPunto4/programa.java
+++ src/actividades/unoPunto4/programa.java
@@ -1,9 +1,14 @@
 package actividades.unoPunto4;
 
 import javax.tools.JavaCompiler;
+import java.io.IOException;
 
 public class programa {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        ProcessBuilder pb = new ProcessBuilder("java", "LeerNombre.java");
+        Process p = pb.start();
+        p.waitFor();
 
 
     }
